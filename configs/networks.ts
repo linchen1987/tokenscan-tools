dc6c3809f279727921393995cb61d7fba215c09b
linchen1987 <linchen.1987@foxmail.com>
2025-02-25 14:31:09 +0800

    network: add ganache local chain

--- configs/networks.ts
+++ configs/networks.ts
@@ -15,21 +15,37 @@ export const ETH_TESTNET_CHAIN_SYMBOL = 'ETH';
 export const ETH_TESTNET_CHAIN_DECIMALS = 18;
 export const ETH_TESTNET_CHAIN_BLOCK_EXPLORER = 'https://sepolia.etherscan.io';
 
+const ganacheSepolia: Chain = {
+  id: 11155111,
+  name: 'Ganache Sepolia',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Sepolia Ether',
+    symbol: 'SepoliaETH',
+  },
+  rpcUrls: {
+    default: { http: ['http://127.0.0.1:7545'] },
+    public: { http: ['http://127.0.0.1:7545'] },
+  },
+};
+
+const hardhat: Chain = {
+  id: 31337,
+  name: 'Hardhat',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: { http: ['http://127.0.0.1:8545'] },
+    public: { http: ['http://127.0.0.1:8545'] },
+  },
+};
+
 export const CHAINS = {
   mainnet,
   flare,
-  hardhat: {
-    id: 31337,
-    name: 'Hardhat',
-    network: 'hardhat',
-    nativeCurrency: {
-      decimals: 18,
-      name: 'Ether',
-      symbol: 'ETH',
-    },
-    rpcUrls: {
-      default: { http: ['http://127.0.0.1:8545'] },
-      public: { http: ['http://127.0.0.1:8545'] },
-    },
-  } as Chain,
+  hardhat,
+  ganacheSepolia,
 };
